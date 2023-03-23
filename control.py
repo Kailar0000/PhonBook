bfa62ee267794056aecd88f88b34581fc9153fa9
Kailar0000 <kailar0000@gmail.com>
2023-03-23 21:47:03 +0300

    "PhonBook"

--- control.py
+++ control.py
@@ -10,23 +10,20 @@ def start():
                 model.open_file()
                 menu.show_message('Файл успешно открыт.')
             case 2:
-                model.save_file()
-                menu.show_message('Файл успешно сохранен.')
-            case 3:
                 menu.show_contacs(pb, 'Телефонная книга пуста или не открыта. ')
-            case 4:
+            case 3:
                 model.add_contact(menu.add_contact())
-            case 5:
+            case 4:
                 if menu.show_contacs(pb, 'Телефонная книга пуста или не открыта. '):
                     index = menu.iput_index('Введите номер изменяемого контакта.')
                     contact = menu.change_contact(pb, index)
                     model.change_contact(contact, index)
                     menu.show_message(f'Контакт {model.get_phone_book()[index-1].get("name")} успешно изменен.')
-            case 6:
+            case 5:
                 search = menu.input_serch('Ведите искомый элемент.')
                 result = model.finde_contact(search)
                 menu.show_contacs(result, 'Контакт не найден.')
-            case 7:
+            case 6:
                 if menu.show_contacs(pb, 'Телефонная книга пуста или не открыта. '):
                     i = input('Ведите номер контакта который хотите удалить: ')
                     if i:
@@ -34,5 +31,7 @@ def start():
                         model.del_contact(i-1)
                     else:
                         pass
-            case 8:
-                return
+            case 7:
+                model.save_file()
+                menu.show_message('Файл успешно сохранен.')
+                break
